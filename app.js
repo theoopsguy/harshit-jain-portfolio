518ce2cc82ea183c366b166133bfa7e42a080678
theoopsguy <theoopsguy01@gmail.com>
2023-08-19 23:31:00 +0530

    Modify the cursor effect text elements

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["build", "work", "develop", "grow"];
+const textArray = ["work", "build", "develop", "grow"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000;
